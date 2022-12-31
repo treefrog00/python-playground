8b1e8bd06870e12de66623ce37bd96df33f62669
James Gregory <james.jrg@gmail.com>
2022-12-31 20:25:37 +0000

    AOC 8.5 - 10, start 11

--- advent-of-code-2022/day8.py
+++ advent-of-code-2022/day8.py
@@ -14,51 +14,36 @@ example_input = """30373
 example_lines = example_input.splitlines()
 
 
-def have_a_look(arr: List[int], reverse: bool):
-    if reverse:
-        start_idx = len(arr) - 1
-        step = -1
-        end = -1
-    else:
-        start_idx = 0
-        step = 1
-        end = len(arr)
+def part_one(matrix):
+    def have_a_look(arr: List[int], reverse: bool):
+        def real_index(idx):
+            return len(arr) - idx - 1 if reverse else idx
 
-    current_height = arr[start_idx]
-    seen = {start_idx}
+        if reverse:
+            arr = list(reversed(arr))
 
-    print(f"reverse {reverse}")
-    for idx in range(start_idx + step, end, step):
-        height = arr[idx]
-        if height > current_height:
-            print(f"idx {idx}, {height} < {current_height}")
-            seen.add(idx)
-        current_height = max(height, current_height)
+        current_height = arr[0]
+        seen = {real_index(0)}
 
-    return seen
+        for idx in range(len(arr)):
+            height = arr[idx]
+            if height > current_height:
+                print(f"idx {idx}, {height} < {current_height}")
+                seen.add(real_index(idx))
+            current_height = max(height, current_height)
 
+        return seen
 
-def print_visible_trees(lines, seen):
-    for r in range(len(lines)):
-        out_chars = []
-        for c in range(len(lines[0])):
-            if (r, c) in seen:
-                out_chars.append("*")
-            else:
-                out_chars.append("x")
-        print(" ".join(out_chars))
-        print("\n")
-
-def run(lines):
-    matrix = []
-    for line in lines:
-        curr_row = []
-        for num in line:
-            curr_row.append(int(num))
-        matrix.append(curr_row)
-
-    for row in matrix:
-        print(row)
+    def print_visible_trees(lines, seen):
+        for r in range(len(lines)):
+            out_chars = []
+            for c in range(len(lines[0])):
+                if (r, c) in seen:
+                    out_chars.append("*")
+                else:
+                    out_chars.append("x")
+            print(" ".join(out_chars))
+            print("\n")
 
     seen = set()
 
@@ -76,11 +61,52 @@ def run(lines):
 
     #print_visible_trees(lines, seen)
 
-    if args.part_one:
-        print(len(seen))
-    else:
-        print("hello")
+    print(f"visible tree count: {len(seen)}")
 
 
-#run(example_lines)
-run(lines)
+def part_two(matrix):
+    def have_a_look(arr: List[int], house_height):
+        count = 0
+        for tree_height in arr:
+            count += 1
+            if tree_height >= house_height:
+                return count
+        return count
+
+    max_scenic_score = 0
+
+    for house_row, row in enumerate(matrix):
+        for house_col in range(len(matrix[0])):
+            leftwards = reversed(row[:house_col])
+            rightwards = row[house_col + 1:]
+            upwards = reversed([row[house_col] for row in matrix[:house_row]])
+            downwards = [row[house_col] for row in matrix[house_row + 1:]]
+
+            house_height = row[house_col]
+
+            print(f"house col {house_col} house_row {house_row} l {leftwards} r {rightwards} up {upwards} down {downwards}")
+
+            scenic_score = 1
+            for arr in [leftwards, rightwards, upwards, downwards]:
+                scenic_score *= have_a_look(arr, house_height)
+
+            if scenic_score > max_scenic_score:
+                max_scenic_score = scenic_score
+
+    print(max_scenic_score)
+
+def build_matrix(lines):
+    matrix = []
+    for line in lines:
+        curr_row = []
+        for num in line:
+            curr_row.append(int(num))
+        matrix.append(curr_row)
+
+    for row in matrix:
+        print(row)
+
+    return matrix
+
+matrix = build_matrix(lines)
+part_one(matrix)
